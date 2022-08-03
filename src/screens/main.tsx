276940100a4afb1e8aaed92487d5ebd5b2dbb0ad
khalid <khalid@khalids-mbp.home>
2022-08-03 19:53:34 +0500

    remove extra code

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,58 +1,14 @@
-import {HomeDrawerParamList, RootStackParamList} from '@/navs';
-import {DrawerScreenProps} from '@react-navigation/drawer';
-import {CompositeScreenProps} from '@react-navigation/native';
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import FeatherIcon from '@/components/icon';
-import {Box, Container} from '@/atoms';
-import {MotiView, View} from 'moti';
-import React from 'react';
-import {StyleSheet} from 'react-native';
 import {Easing} from 'react-native-reanimated';
-
-type Props = CompositeScreenProps<
-  DrawerScreenProps<HomeDrawerParamList, 'Main'>,
-  NativeStackScreenProps<RootStackParamList>
->;
+import FeatherIcon from '@/components/icon';
+import {StyleSheet} from 'react-native';
+import {Box, Container} from '@/atoms';
+import {MotiView} from 'moti';
+import React from 'react';
 
 const _color = '#6E01Ef';
 const _size = 100;
 
-const PhoneRing = ({size}: {size: number}) => (
-  <MotiView
-    from={{
-      width: size,
-      height: size,
-      borderRadius: size / 2,
-      borderWidth: 0,
-      shadowOpacity: 0.5,
-    }}
-    animate={{
-      width: size + 20,
-      height: size + 20,
-      borderRadius: (size + 20) / 2,
-      borderWidth: size / 10,
-      shadowOpacity: 1,
-    }}
-    transition={{
-      type: 'timing',
-      duration: 500,
-      loop: true,
-    }}
-    style={{
-      width: size,
-      height: size,
-      borderRadius: size / 2,
-      borderWidth: size / 10,
-      borderColor: '#fff',
-      shadowColor: '#fff',
-      shadowOffset: {width: 0, height: 0},
-      shadowOpacity: 1,
-      shadowRadius: 10,
-    }}
-  />
-);
-
-export default function MainScreen({navigation}: Props) {
+export default function MainScreen() {
   return (
     <Container
       justifyContent="center"
