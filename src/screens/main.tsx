1eef29b294526a1fb7a26e1f57a222bc82ddd782
khalid <khalid@khalids-mbp.home>
2022-08-07 02:47:00 +0500

    color interpolate

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,13 +1,113 @@
 import React from 'react';
+import {Switch, StyleSheet, Dimensions} from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  interpolateColor,
+  useDerivedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import AnimatedBox from '@/components/AnimatedBox';
 
-import {Container, Text} from '@/atoms';
+const Colors = {
+  dark: {
+    background: '#1E1E1E',
+    circle: '#252525',
+    text: '#F8F8F8',
+  },
+  light: {
+    background: '#F8F8F8',
+    circle: '#FFF',
+    text: '#1E1E1E',
+  },
+};
+
+type Theme = 'light' | 'dark';
+
+const SWITCH_TRACK_COLOR = {
+  true: 'rgba(256,0,256,0.2)',
+  false: 'rgba(0,0,0,0.2)',
+};
 
 export default function MainScreen() {
+  const [theme, setTheme] = React.useState<Theme>('light');
+  const progress = useDerivedValue(() => {
+    return theme === 'dark' ? withTiming(1) : withTiming(0);
+  }, [theme]);
+
+  const rStyle = useAnimatedStyle(() => {
+    const backgroundColor = interpolateColor(
+      progress.value,
+      [0, 1],
+      [Colors.light.background, Colors.dark.background],
+    );
+
+    return {backgroundColor};
+  });
+
+  const rCircleStyle = useAnimatedStyle(() => {
+    const backgroundColor = interpolateColor(
+      progress.value,
+      [0, 1],
+      [Colors.light.circle, Colors.dark.circle],
+    );
+
+    return {backgroundColor};
+  });
+
+  const rTextStyle = useAnimatedStyle(() => {
+    const color = interpolateColor(
+      progress.value,
+      [0, 1],
+      [Colors.light.text, Colors.dark.text],
+    );
+
+    return {color};
+  });
+
   return (
-    <Container justifyContent="center" alignItems="center">
-      <Text letterSpacing={2} fontWeight="700" fontSize={30}>
-        Animations
-      </Text>
-    </Container>
+    <AnimatedBox
+      flex={1}
+      justifyContent="center"
+      alignItems="center"
+      style={rStyle}>
+      <Animated.Text style={[styles.text, rTextStyle]}>Theme</Animated.Text>
+      <Animated.View style={[styles.circle, rCircleStyle]}>
+        <Switch
+          value={theme === 'dark'}
+          onValueChange={toggled => {
+            setTheme(toggled ? 'dark' : 'light');
+          }}
+          trackColor={SWITCH_TRACK_COLOR}
+          thumbColor={'violet'}
+        />
+      </Animated.View>
+    </AnimatedBox>
   );
 }
+
+const SIZE = Dimensions.get('window').width * 0.5;
+
+const styles = StyleSheet.create({
+  circle: {
+    width: SIZE,
+    height: SIZE,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: SIZE / 2,
+    shadowOffset: {
+      width: 0,
+      height: 20,
+    },
+    shadowRadius: 10,
+    shadowOpacity: 0.1,
+    elevation: 8,
+  },
+  text: {
+    fontSize: 40,
+    textTransform: 'uppercase',
+    fontWeight: '700',
+    letterSpacing: 10,
+    marginBottom: 15,
+  },
+});
